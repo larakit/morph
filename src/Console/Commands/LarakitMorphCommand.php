60af5a8779987e2ffd15d60e0267739ad5fd00b4
Алексей Бердников <aberdnikov@gmail.com>
2020-03-17 13:35:42 +0500

    Заготовка morph-модуля

---
+++ src/Console/Commands/LarakitMorphCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Larakit\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
+use Larakit\Helpers\HelperText;
+use Larakit\LangManager;
+
+class LarakitMorphCommand extends Command {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'larakit:morph';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Генерация ключей моделей для модуля morph';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle() {
+        $ret = [];
+        foreach (get_declared_classes() as $class) {
+            if (is_subclass_of($class, Model::class)) {
+                $key       = md5($class);
+                $ret[$key] = $class;
+            }
+        }
+        $file = base_path('config/larakit-morph.php');
+        file_put_contents($file, '<?php' . PHP_EOL . 'return ' . var_export($ret, true) . ';');
+        $this->info('Сгенерирован morph-конфиг для ' . HelperText::plural_with_number(count($ret), ' модель', ' модели', 'моделей'));
+    }
+
+}
---
+++ src/MorphServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Larakit;
+
+use Larakit\Console\Commands\LarakitMorphCommand;
+
+class MorphServiceProvider extends \Illuminate\Support\ServiceProvider {
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot() {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../publishes/' => base_path('/'),
+            ]);
+        }
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register() {
+
+        //        \Route::middleware('api')
+        //              ->prefix('api')
+        //              ->namespace('Larakit\Controllers')
+        //              ->group(function () {
+        //                  foreach (rglob('*.php', 0, __DIR__ . '/../routes/') as $file) {
+        //                      include_once $file;
+        //                  };
+        //              });
+        $this->app->bind('larakit:morph', LarakitMorphCommand::class);
+        $this->commands([
+            'larakit:morph',
+        ]);
+    }
+}
